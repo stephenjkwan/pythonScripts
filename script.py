52503fa5b9af9f148d86cc25e18242fb4cd04997
EC2 Default User <ec2-user@ip-172-31-69-210.ec2.internal>
2017-05-19 19:07:22 +0000

    initial commit

---
+++ script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import boto3
+import time
+
+s3 = boto3.resource('s3')
+cronFile = open('/var/log/cron','rb')
+s3.Bucket('stephenkwansbucket').put_object(Key='cron', Body=cronFile)
+   #time.sleep(600)
+
